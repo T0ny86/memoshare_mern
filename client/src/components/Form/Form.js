32096c0c02e1b26164ab5c13c305a2ba16b6660a
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-10 22:59:00 +0200

    add basci components, and styles using material-ui

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from "react"
+import useStyles from "./styles"
+
+const From = ()=>{
+const classes = useStyles();
+
+    return(
+        <h1>form</h1>
+    )
+}
+
+export default From;
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,17 +1,30 @@
 import PostMessage from "../models/postMessage.js"
 
-export const getPosts = (req, res)=>{
-    res.send("this works")
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find()
+        res.status(200).json(postMessages)
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message })
+    }
 }
 
-export const createPost = (req,res) =>{
-    res.send("post creation")
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
 
-export const deletePost = (req,res) =>{
+export const deletePost = (req, res) => {
     res.send("post deleted")
 }
 
-export const updatePost = (req,res) =>{
+export const updatePost = (req, res) => {
     res.send("post updated")
 }
