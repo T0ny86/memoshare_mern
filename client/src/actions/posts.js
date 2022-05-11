47e3387db6b9e7e7b0f181da1a21d7b6cabcbc75
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-11 12:00:02 +0200

    add:action creator: getPosts, to dispatch the action

---
+++ client/src/actions/posts.js
@@ -0,0 +1,18 @@
+import * as api from "../api";
+
+/* Action Creators: are functions that return Actions 
+to handle asynchronous fetch data, we need to use 'redux-thunk'.
+the usage like bellow " async(dispatch)=>{} "
+this function return/or dispatch a function, so we can use async in Action Creator
+and at the end we can "dispatch(action)", instead of "return action"
+*/
+// ref: https://youtu.be/VsUzmlZfYNg?t=3630
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data })
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
