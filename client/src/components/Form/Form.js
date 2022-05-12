de63d8b79b4b9a5e78ace11f78601e5fb59e5eb2
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-12 22:13:15 +0200

    fix some mistakes

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,21 +1,28 @@
 import React, { useState } from "react"
-import useStyles from "./styles"
 import { TextField, Button, Typography, Paper } from "@material-ui/core"
 import FileBase from "react-file-base64"
+import { useDispatch } from "react-redux";
+
+import { createPost } from "../../actions/posts";
+import useStyles from "./styles"
+
 
 const From = () => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
-        messsage: '',
+        message: '',
         tags: '',
         selectedFile: '',
     });
+    const dispatch = useDispatch();
 
     const classes = useStyles();
 
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        // console.log('postData:', postData)
+        e.preventDefault();
+        dispatch(createPost(postData))
     }
 
     const clear = () => {
@@ -30,18 +37,22 @@ const From = () => {
                     value={postData.creator}
                     onChange={(e) => setPostData({ ...postData, creator: e.target.value })}
                 />
+
                 <TextField name="title" variant="outlined" label="Title" fullWidth
                     value={postData.title}
                     onChange={(e) => setPostData({ ...postData, title: e.target.value })}
                 />
+
                 <TextField name="message" variant="outlined" label="<Message>" fullWidth
                     value={postData.message}
                     onChange={(e) => setPostData({ ...postData, message: e.target.value })}
                 />
+
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth
                     value={postData.tags}
                     onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
                 />
+
                 <div className={classes.fileInput}>
                     <FileBase
                         type="file"
@@ -54,7 +65,7 @@ const From = () => {
                     variant="contained" color="primary" size="large" type="submit" fullWidth
                 > Submit </Button>
 
-                <Button variant="contained" color="secondary" size="small" type="submit" onClick={clear} fullWidth
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth
                 > Clear </Button>
 
             </form>
