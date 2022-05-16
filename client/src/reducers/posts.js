82eb7184696ce9bd24398173ac39e8f0a1e087ab
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-16 22:24:39 +0200

    add constants/actionTypes.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,20 +1,21 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from "../constants/actionTypes";
 // state/post must be al always something, so assigning default value
 // reducer function:
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
 
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
 
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
 
-        case 'DELETE':
-            return posts.filter((post) => post._id != action.payload); // payload content only the 'id' value, look at line:42 in actions (deletePost)
-            
+        case DELETE:
+            return posts.filter((post) => post._id !== action.payload); // payload content only the 'id' value, look at line:42 in actions (deletePost)
+
         default:
             return posts;
     }
