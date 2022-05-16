c1def685331a6f2b716afc6bbcee6f88c21c4585
Tanous Atallah <tanous.atallah@gmail.com>
2022-05-16 22:35:23 +0200

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express"
-import bodyParser from "body-parser"
+// import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 import postRouters from "./routers/posts.js"
-
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
 // you don't need to install additional body-parser package, it ab ver:4.16 the express has own parser
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -14,11 +15,9 @@ app.use(cors());
 
 app.use('/posts', postRouters)
 
-
-const CONNECTION_URL = "mongodb+srv://memos:memos@cluster0.d33db.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port:${PORT}`)))
     .catch((err) => console.log(err.message))
 
